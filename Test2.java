93c20af8f41c21b3acbc495c05d68736d9a85b35
saiprasadkilaru <kilarusaiprasad19@gmail.com>
2024-01-30 14:28:53 +0530

    changes committed

---
+++ Test2.java
@@ -0,0 +1,9 @@
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Byte.*;
+
+class Test2{
+//static final int MAX_VALUE=999;
+public static void main(String[] args){
+		System.out.println(MAX_VALUE);
+	}	
+}
