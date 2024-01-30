61ee8f1850b864189175a7a28ef2564d05676ba8
saiprasadkilaru <kilarusaiprasad19@gmail.com>
2024-01-30 14:15:19 +0530

    changes committed

---
+++ Test.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+import java.sql.*;
+class Test{
+	public static void main(String[] args){
+		Date d = new Date();
+		System.out.println("Date: "+d.toLocaleString());
+	}
+}
---
+++ Test1.java
@@ -0,0 +1,7 @@
+import static java.lang.System.out;
+class Test1{
+	public static void main(String[] args){
+		out.println("Hello");
+		out.println("Hi");
+	}
+}
